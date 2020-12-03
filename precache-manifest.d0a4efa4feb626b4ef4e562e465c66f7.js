3eab8d3ffcee3493935a748620bfd874f11c8ca8
Vivek Kumar <vivekkumar171000@gmail.com>
2020-12-03 15:02:08 +0530

    Updates

---
+++ precache-manifest.d0a4efa4feb626b4ef4e562e465c66f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24123607e06548eacfcfb7dbd2668c4",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "22bc6301b30407ccf7bd",
+    "url": "/covid19_tracker/static/css/main.4c16e6da.chunk.css"
+  },
+  {
+    "revision": "2cc05aa9b54971c1f7e8",
+    "url": "/covid19_tracker/static/js/2.1d314e52.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19_tracker/static/js/2.1d314e52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22bc6301b30407ccf7bd",
+    "url": "/covid19_tracker/static/js/main.55830de8.chunk.js"
+  },
+  {
+    "revision": "2e982127077a4e2382cf",
+    "url": "/covid19_tracker/static/js/runtime-main.aef5a27c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19_tracker/static/media/image.d7265326.png"
+  }
+]);
